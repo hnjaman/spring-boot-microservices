2b0877065e1035087a560b6bdf4e55c7ef60ceca
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-03-05 12:06:50 +0600

    Nid and passport entity

---
+++ src/main/java/com/example/demo/entities/Nid.java
@@ -0,0 +1,82 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotBlank;
+@Entity
+public class Nid {
+    @NotBlank(message="Enter your first name")
+    private String fname;
+    @NotBlank(message="Enter your last name")
+    private String lname;
+    @Id
+    private Long nid;
+    @NotBlank(message="Enter your name")
+    private String ppid;
+    @NotBlank(message="Enter your name")
+    private String birthdate;
+    
+    
+    
+    
+	public Nid(String fname, String lname, Long nid, String ppid, String birthdate) {
+		this.fname = fname;
+		this.lname = lname;
+		this.nid = nid;
+		this.ppid = ppid;
+		this.birthdate = birthdate;
+	}
+	
+	
+	
+	public Nid() {
+		
+	}
+
+	
+	
+
+
+	@Override
+	public String toString() {
+		return "Nid [fname=" + fname + ", lname=" + lname + ", nid=" + nid + ", ppid=" + ppid + ", birthdate="
+				+ birthdate + "]";
+	}
+
+
+
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public Long getNid() {
+		return nid;
+	}
+	public void setNid(Long nid) {
+		this.nid = nid;
+	}
+	public String getPpid() {
+		return ppid;
+	}
+	public void setPpid(String ppid) {
+		this.ppid = ppid;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+    
+	
+}
---
+++ src/main/java/com/example/demo/entities/Passport.java
@@ -0,0 +1,89 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+public class Passport {
+
+	 @NotBlank(message="Enter your first name")
+	    private String fname;
+	    @NotBlank(message="Enter your last name")
+	    private String lname;
+	    @NotBlank(message="Enter your email")
+	    @Id
+	    @Email(message="Enter a valid mail")
+	    private String email;
+	    @Id
+	    private Long nid;
+	    @NotBlank(message="Enter your name")
+	    private String ppid;
+	    @NotBlank(message="Enter your name")
+	    private String birthdate;
+	    
+	    
+	    
+	    
+	    
+		@Override
+		public String toString() {
+			return "Passport [fname=" + fname + ", lname=" + lname + ", email=" + email + ", nid=" + nid + ", ppid="
+					+ ppid + ", birthdate=" + birthdate + "]";
+		}
+		public Passport() {
+			
+		}
+		public Passport(String fname, String lname, String email, Long nid, String ppid, String birthdate) {
+			this.fname = fname;
+			this.lname = lname;
+			this.email = email;
+			this.nid = nid;
+			this.ppid = ppid;
+			this.birthdate = birthdate;
+		}
+		
+		public String getFname() {
+			return fname;
+		}
+		public void setFname(String fname) {
+			this.fname = fname;
+		}
+		public String getLname() {
+			return lname;
+		}
+		public void setLname(String lname) {
+			this.lname = lname;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public Long getNid() {
+			return nid;
+		}
+		public void setNid(Long nid) {
+			this.nid = nid;
+		}
+		public String getPpid() {
+			return ppid;
+		}
+		public void setPpid(String ppid) {
+			this.ppid = ppid;
+		}
+		public String getBirthdate() {
+			return birthdate;
+		}
+		public void setBirthdate(String birthdate) {
+			this.birthdate = birthdate;
+		}
+	    
+	    
+	    
+	    
+	
+}
