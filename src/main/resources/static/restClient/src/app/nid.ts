f95a7fecd8503d9a2b222c8b32dc6419e2381944
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-03-06 02:05:39 +0600

    new file

---
+++ src/main/resources/static/restClient/src/app/nid.ts
@@ -0,0 +1,8 @@
+export class Nid {
+    id:Number;
+    fname:string;
+    lname:string;
+    nid:string;
+    ppid:string;
+    birthdate:string;
+}
---
+++ src/main/resources/static/restClient/src/app/passport.ts
@@ -0,0 +1,9 @@
+export class Passport {
+    id:Number;
+    fname:string;
+    lname:string;
+    email:string;
+    nid:string;
+    ppid:string;
+    birthdate:string;
+}
