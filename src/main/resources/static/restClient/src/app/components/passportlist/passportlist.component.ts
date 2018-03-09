bf51be572dde4520db1ddda5fa64c93643d5ee7f
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-03-10 03:51:34 +0600

    all passport list view

--- src/main/resources/static/restClient/src/app/components/passportlist/passportlist.component.ts
+++ src/main/resources/static/restClient/src/app/components/passportlist/passportlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Passport } from '../../passport';
+import { UserService } from '../../shared-service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-passportlist',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./passportlist.component.css']
 })
 export class PassportlistComponent implements OnInit {
-
-  constructor() { }
+private passports:Passport[];
+  constructor(private _userService:UserService, private _router:Router ) { }
 
   ngOnInit() {
+    this._userService.getPassports().subscribe((passports)=>{
+      console.log(passports);
+      this.passports=passports;
+    },(error)=>{
+      console.log(error);
+    })
   }
+
+  /********************* Passport actions */
+
+deletePassport(passport){
+  this._userService.deletePassport(passport.id).subscribe((data)=>{
+    this.passports.splice(this.passports.indexOf(passport),1);
+  },(error)=>{
+    console.log(error);
+  });
+}
+
+updatePassport(passport){
+  this._userService.setterPassport(passport);
+  this._router.navigate(['/passport']);
+}
+
+createPassport(){
+  let passport = new Passport();
+  this._userService.setterPassport(passport);
+  this._router.navigate(['/passport'])
+}
+
 
 }
