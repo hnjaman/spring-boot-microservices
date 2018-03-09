0223fd301e89cc3b2c75b1e228421c0b02549e38
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-03-10 03:50:27 +0600

    all nid list view

--- src/main/resources/static/restClient/src/app/components/nidlist/nidlist.component.ts
+++ src/main/resources/static/restClient/src/app/components/nidlist/nidlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared-service/user.service';
+import { Router } from '@angular/router';
+import { Nid } from '../../nid';
 
 @Component({
   selector: 'app-nidlist',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nidlist.component.css']
 })
 export class NidlistComponent implements OnInit {
-
-  constructor() { }
+private nids:Nid[];
+  constructor(private _userService:UserService, private _router:Router) { }
 
   ngOnInit() {
+    this._userService.getNids().subscribe((nids)=>{
+      console.log(nids);
+      this.nids=nids;
+    },(error)=>{
+      console.log(error);
+    })
   }
+
+  
+/** Nid actions */
+
+
+deleteNid(nid){
+  this._userService.deleteNid(nid.id).subscribe((data)=>{
+    this.nids.splice(this.nids.indexOf(nid),1);
+  },(error)=>{
+    console.log(error);
+  });
+}
+
+updateNid(nid){
+  this._userService.setterNid(nid);
+  this._router.navigate(['/nid']);
+}
+
+createNid(){
+  let nid = new Nid();
+  this._userService.setterNid(nid);
+  this._router.navigate(['/nid']);
+}
+
 
 }
