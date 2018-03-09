bcd612e36d9eb04db4b5118eccb53012f0647cca
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-03-10 03:55:14 +0600

    service actions

--- src/main/resources/static/restClient/src/app/shared-service/user.service.ts
+++ src/main/resources/static/restClient/src/app/shared-service/user.service.ts
@@ -4,6 +4,8 @@ import{Observable}   from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+
+
 import{User}  from '../user';
 import { Nid } from '../nid';
 import { Passport } from '../passport';
@@ -12,16 +14,20 @@ import { Passport } from '../passport';
 
 @Injectable()
 export class UserService {
+  
   private baseUrl:string='http://localhost:8888/api';
   private headers = new Headers({'Content-Type':'application/json'});
   private options = new RequestOptions({headers:this.headers});
+  
   private user = new User();
   private nid = new Nid();
   private passport = new Passport();
+
   constructor(private _http:Http) { }
 
 
   
+  /************************* Nid */
 
   getNids(){
     return this._http.get(this.baseUrl+'/nids',this.options).map((response:Response)=>response.json())
@@ -33,6 +39,25 @@ export class UserService {
       .catch(this.errorHandler);
   }
 
+  deleteNid(id:Number){
+    return this._http.delete(this.baseUrl+'/nids/'+id,this.options).map((response:Response)=>response.json())
+  .catch(this.errorHandler);
+  }
+
+  updateNid(nid:Nid){
+    return this._http.put(this.baseUrl+'/nid',JSON.stringify(nid), this.options).map((response:Response)=>response.json())
+  .catch(this.errorHandler);
+  }
+
+  createNid(nid:Nid){
+    return this._http.post(this.baseUrl+'/nid',JSON.stringify(nid), this.options).map((response:Response)=>response.json())
+  .catch(this.errorHandler);
+  }
+
+  
+
+  /*********************************** Passport */
+
   getPassports(){
     return this._http.get(this.baseUrl+'/passports',this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
@@ -42,6 +67,25 @@ export class UserService {
     return this._http.get(this.baseUrl+'/passports/'+id,this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
   }
+
+  deletePassport(id:Number){
+    return this._http.delete(this.baseUrl+'/passports/'+id,this.options).map((response:Response)=>response.json())
+  .catch(this.errorHandler);
+  }
+
+  updatePassport(passport:Passport){
+    return this._http.put(this.baseUrl+'/passport',JSON.stringify(passport),this.options).map((response:Response)=>response.json())
+  .catch(this.errorHandler);
+  }
+
+  createPassport(passport:Passport){
+    return this._http.post(
+      this.baseUrl+'/passport',JSON.stringify(passport),this.options
+    ).map((response:Response)=>response.json())
+  .catch(this.errorHandler);
+  }
+
+  /********************************** Users */
 
   getUsers(){
     return this._http.get(this.baseUrl+'/users',this.options).map((response:Response)=>response.json())
@@ -72,11 +116,28 @@ export class UserService {
      return Observable.throw(error||"SERVER ERROR");
   }
 
-setter(user:User){
-     this.user=user;
-}
-
-getter(){
-    return this.user;
+  setter(nid:Nid){
+      this.nid=nid;
   }
+
+  getter(){
+    return this.nid;
+  }
+
+  setterNid(nid:Nid){
+    this.nid=nid;
+  }
+
+  getterNid(){
+    return this.nid;
+  }
+
+  setterPassport(passport:Passport){
+    this.passport=passport;
+  }
+
+  getterPassport(){
+    return this.passport;
+  } 
+
 }
