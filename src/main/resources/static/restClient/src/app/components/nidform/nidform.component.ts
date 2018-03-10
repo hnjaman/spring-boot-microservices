fff44bf3b7131aaaef9455c69ad3b8776bfe671a
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-03-11 02:55:38 +0600

    nid form validation

--- src/main/resources/static/restClient/src/app/components/nidform/nidform.component.ts
+++ src/main/resources/static/restClient/src/app/components/nidform/nidform.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Nid } from '../../nid';
 import { UserService } from '../../shared-service/user.service';
 import { Router } from '@angular/router';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-nidform',
@@ -9,29 +10,60 @@ import { Router } from '@angular/router';
   styleUrls: ['./nidform.component.css']
 })
 export class NidformComponent implements OnInit {
-private nid:Nid;
+
+ /** private nid:Nid; */
+
+  private nid = new Nid();
+
+  private nidError:Nid;
+  private isNidCreated:boolean=false;
+  private nidExist:boolean=false;
+
   constructor(private _userService:UserService,private _rotuer:Router) { }
 
   ngOnInit() {
-    this.nid=this._userService.getter();
+    this.nid=this._userService.getterNid();
   }
 
-  processNidForm(){
-    if(this.nid.id==undefined){
-       this._userService.createNid(this.nid).subscribe((nid)=>{
-         console.log(nid);
-         this._rotuer.navigate(['/']);
-       },(error)=>{
-         console.log(error);
-       });
-    }else{
-       this._userService.updateNid(this.nid).subscribe((nid)=>{
-         console.log(nid);
-         this._rotuer.navigate(['/']);
-       },(error)=>{
-         console.log(error);
-       });
+
+  createNid(){
+    this._userService.createNid(this.nid).subscribe(
+      data=>{
+        console.log(data);
+        this.nid=new Nid();
+        this.isNidCreated=true;
+        this.nidExist=false;
+        this.nidError= new Nid();
+      },
+      error=>{
+        this.nidError=error.error;
+        this.isNidCreated=false;
+        if(error.status==409){
+          this.isNidCreated=false;
+          this.nidExist=true;
+        }
+        console.log(error);
+      }
+    )
+  }
+  
+  /**
+    processNidForm(){
+      if(this.nid.id==undefined){
+        this._userService.createNid(this.nid).subscribe((nid)=>{
+          console.log(nid);
+          this._rotuer.navigate(['/']);
+        },(error)=>{
+          console.log(error);
+        });
+      }else{
+        this._userService.updateNid(this.nid).subscribe((nid)=>{
+          console.log(nid);
+          this._rotuer.navigate(['/']);
+        },(error)=>{
+          console.log(error);
+        });
+      }
     }
-  }
-
+  */
 }
