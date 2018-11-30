bfc7904ee9bb6523d00ef24aa79ae3e47fe53917
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-11-30 19:16:11 +0600

    multiple profiles for location service

--- spring-cloud-config-server/src/main/java/com/hnj/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/hnj/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -1,10 +1,13 @@
 package com.hnj.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
