2af281b0446a9b49694d995545168dee5a37c775
Hasan Noor Jaman <hnjsujon.mbstu@gmail.com>
2018-12-03 11:28:07 +0600

    dummy data insert allow

--- contact-service/src/main/java/com/hnj/contactservice/ContactServiceApplication.java
+++ contact-service/src/main/java/com/hnj/contactservice/ContactServiceApplication.java
@@ -15,8 +15,8 @@ public class ContactServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        iContact.save( new Contact("01789032651","hnjaman", "dhaka"));
-//        iContact.save( new Contact("01521238635","hnjsujon", "jhenaidah"));
+        iContact.save( new Contact("01789032651","hnjaman", "dhaka"));
+        iContact.save( new Contact("01521238635","hnjsujon", "jhenaidah"));
     }
 
     public static void main(String[] args) {
