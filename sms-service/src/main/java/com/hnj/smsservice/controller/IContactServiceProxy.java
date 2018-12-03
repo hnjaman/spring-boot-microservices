cf647855c9aa8be801816c8b651335d44e5ca2b4
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-12-04 01:56:14 +0600

    feign with request mapping not woking

--- sms-service/src/main/java/com/hnj/smsservice/controller/IContactServiceProxy.java
+++ sms-service/src/main/java/com/hnj/smsservice/controller/IContactServiceProxy.java
@@ -3,10 +3,13 @@ package com.hnj.smsservice.controller;
 import com.hnj.smsservice.entities.Sms;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "contact-service", url = "localhost:8555")
 public interface IContactServiceProxy {
-    @GetMapping("/contacts-feign/name/{name}/sms")
+    @RequestMapping(method = RequestMethod.GET, value = "/contacts-feign/name/{name}/sms")
+    //@GetMapping("/contacts-feign/name/{name}/sms")
     Sms getUserSms(@PathVariable("name") String name);
 }
